c857133f9867c71d43c0dc021f2b449f1c100eb9
Provost Simon <simon1.provost@epitech.eu>
2025-09-19 15:22:40 +0100

    feat: add pypi CD

--- src/mcpstack_autoddg/__init__.py
+++ src/mcpstack_autoddg/__init__.py
@@ -3,9 +3,12 @@ from .core.state import ToolState
 from .services.autoddg_service import AutoDDGService
 from .tool import AutoDDGTool
 
+__version__ = "0.0.1"
+
 __all__ = [
     "AutoDDGService",
     "AutoDDGTool",
     "ToolState",
     "sample_dataframe",
+    "__version__",
 ]
